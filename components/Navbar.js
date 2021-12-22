9e9d3fd720adbce8726826e06cc2ae63bab758ed
gervie <gerviebarczyk@gmail.com>
2021-12-22 21:48:51 +0100

    initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <Image
+          src="/ninja-logo.png"
+          alt="ninja logo"
+          width={108}
+          height={100}
+        />
+      </div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/ninjas">
+        <a>Nina Listing</a>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
